5e6a00aabe96b0cd1f9abcc930574e46c9124d28
rlawncks125 <rlawncks125@naver.com>
2020-11-27 14:01:17 +0900

    swiper 까지 추가했음

--- 첫번째/main.js
+++ 첫번째/main.js
@@ -28,6 +28,18 @@ function GetTxtWdith(text){
 }
 
 
+// const swiper = new Swiper('.swiper-container', {
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
 
-
-
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
