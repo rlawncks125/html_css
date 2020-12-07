9da40b13f383f7709d00ea720486f332d0781659
rlawncks125 <rlawncks125@naver.com>
2020-12-08 05:08:19 +0900

    부트스트랩 class로 하다가 CSS쪽에 자꾸 꼬임 일단 중단

--- 두번째/main.js
+++ 두번째/main.js
@@ -1,3 +1,25 @@
 
 "use strict";
-
+//  Swiper
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    effect: 'fade',
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    //   type : 'fraction',
+        // renderBullet: function (index, className) {
+        // return `<span class="${className}">${index+1}</span>`;
+        // }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
